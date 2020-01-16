522c602c9c5ac2da94aba24c8482c858359ad183
Jiajie <Jiajie@123.com>
2020-01-16 09:59:26 +0800

    First

---
+++ src/com/ithiema/test.java
@@ -0,0 +1,11 @@
+package com.ithiema;
+
+/**
+ * @Author:Jiajie
+ * @Date:2020/1/169:52
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
